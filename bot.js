daad75185ad6f18f536485c4260589506fd86893
benk1960yt <40948605+benk1960yt@users.noreply.github.com>
2018-07-11 17:06:16 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+client.on('message', function(message) {
+    if (message.content == "$clear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
+        }
+    }
+
+});
+
+client.login(process.env.BOT_TOKEN);
