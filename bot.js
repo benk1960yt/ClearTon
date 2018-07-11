66b13891775b4732ce4e1112c6d7edec076d113c
benk1960yt <40948605+benk1960yt@users.noreply.github.com>
2018-07-11 17:16:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,14 +9,14 @@ client.on('ready', () => {
 
 
 client.on('message', function(message) {
-    if (message.content == "$clear") {
-        if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.channel.fetchMessages()
-               .then(function(list){
-                    message.channel.bulkDelete(list);
-                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
-        }
+  if (message.content == "$clear") {
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+      message.channel.fetchMessages()
+        .then(function(list){
+          message.channel.bulkDelete(list);
+      }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
     }
+  }
 
 });
 
